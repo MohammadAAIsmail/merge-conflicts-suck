7f3e34e02ce98875e169bd515da46ccfa9e543bb
MohammadAAIsmail <38306627+MohammadAAIsmail@users.noreply.github.com>
2018-07-08 19:58:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 var num = 50;
-<<<<<<< HEAD
-var truth = "Hello Flatter Earth";
-=======
-var truth = "Hello Flat Earth";
->>>>>>> 391d4a389ab17aae5adab1016b9a029374a6aafb
+var truth = "Hello World";
